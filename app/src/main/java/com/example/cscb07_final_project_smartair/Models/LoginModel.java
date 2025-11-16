441139ab20ded7d331eb399f921657e5f129d010
Lyronne Sealy <lyronne78@gmail.com>
2025-11-16 04:22:33 -0500

    Implemented Login, SignUp & Recovery

---
+++ app/src/main/java/com/example/cscb07_final_project_smartair/Models/LoginModel.java
@@ -0,0 +1,70 @@
+package com.example.cscb07_final_project_smartair.Models;
+
+import androidx.annotation.NonNull;
+
+import com.example.cscb07_final_project_smartair.Presenters.LoginPresenter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginModel {
+
+    private FirebaseAuth mAuth;
+
+    public interface OnResetPasswordFinishedListener {
+        void onResetPasswordSuccess(String message);
+
+        void onResetPasswordFailure(String errorMessage);
+    }
+
+    public LoginModel() {
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public interface OnLoginFinishedListener {
+        void onLoginSuccess();
+        void onLoginFailure(String errorMessage);
+    }
+
+
+
+    public void signInUser(String email, String password, OnLoginFinishedListener listener) {
+        // Credential Validation by Liam
+
+        //Assuming Credentials are valid
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            listener.onLoginSuccess();
+                        } else {
+                            String errorMessage = task.getException() != null ?
+                                    task.getException().getMessage() : "Authentication failed.";
+                            listener.onLoginFailure(errorMessage);
+                        }
+                    }
+                });
+    }
+
+    public void sendPasswordResetEmail(String email, OnResetPasswordFinishedListener listener) {
+        //email validation to go here.
+
+        mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            listener.onResetPasswordSuccess("Password reset email sent. Please check your email.");
+                        } else {
+                            String errorMessage = task.getException() != null ?
+                                    task.getException().getMessage() : "Password reset failed.";
+                            listener.onResetPasswordFailure(errorMessage);
+                        }
+                    }
+                });
+    }
+}
+
+
