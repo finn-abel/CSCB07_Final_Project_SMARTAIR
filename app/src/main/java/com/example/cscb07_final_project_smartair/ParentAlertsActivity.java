9b3e6a3aa99dcdb37176519abf7249abe114b75e
Prathab <prathabbhatti@gmail.com>
2025-11-25 21:40:25 -0500

    Updated feature/R6_Alerts

--- app/src/main/java/com/example/cscb07_final_project_smartair/ParentAlertsActivity.java
+++ app/src/main/java/com/example/cscb07_final_project_smartair/ParentAlertsActivity.java
@@ -1,17 +1,11 @@
 package com.example.cscb07_final_project_smartair;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -20,7 +14,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.Objects;
 
 public class ParentAlertsActivity extends AppCompatActivity {
@@ -32,7 +25,7 @@ public class ParentAlertsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parent_home);
+        setContentView(R.layout.activity_parent_alerts);
 
         // Get a database instance
         mdatabase = FirebaseDatabase.getInstance().getReference();
