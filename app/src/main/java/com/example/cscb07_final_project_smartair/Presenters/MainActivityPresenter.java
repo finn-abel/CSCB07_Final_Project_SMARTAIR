cf9470a7941f489bbab4057d041f1a4289fc9bce
Prathab <prathabbhatti@gmail.com>
2025-11-25 21:27:47 -0500

    Updated feature/R6_Dashboard_Tiles

--- app/src/main/java/com/example/cscb07_final_project_smartair/Presenters/MainActivityPresenter.java
+++ app/src/main/java/com/example/cscb07_final_project_smartair/Presenters/MainActivityPresenter.java
@@ -1,6 +1,7 @@
 package com.example.cscb07_final_project_smartair.Presenters;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.cscb07_final_project_smartair.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivityPresenter extends AppCompatActivity {
 
@@ -22,5 +25,20 @@ public class MainActivityPresenter extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Firebase test - to delete later
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("test_message");
+
+        myRef.setValue("Test Firebase 3").addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Log.d("FirebaseTest", "Data written successfully");
+            } else {
+                Log.e("FirebaseTest", "Failed to write data", task.getException());
+            }
+        });
+
+        // End of test
     }
 }
